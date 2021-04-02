7566668c8712a0406bd583555728dd577adfb5dd
dagrons <heyuehuii@126.com>
2021-04-02 16:48:45 +0800

    delete all but knowledge graph.

--- backend/index.js
+++ backend/index.js
@@ -1,74 +1,29 @@
+// config 
+const neo4j_ip_addr = '172.29.236.17';
+const neo4j_port = '7687';
+const listen_port = '3000';
+const username = 'neo4j';
+const password = 'daxiahyh';
+
+// imports
 const Koa = require('koa');
-const router = require('koa-router')();
-const app = new Koa();
-const all = require('./all');
+const cors = require('koa2-cors'); // plugin for cors
 
-const os = require('os');
-
-var cors = require('koa2-cors'); // CORS是一个W3C标准，全称是"跨域资源共享"
-app.use(cors());
-
-var myHost = '';
-
-function getIPAdd() {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-        //将'WLAN'替换为系统所处网络
-        if (dev.includes('WLAN')) {
-            break;
-        }
+// Koa
+const app = new Koa()
+app.use(cors()); // use cors to bypass protection from browsers
+app.use(async ctx => {
+    const query = ctx.query.query;
+    const neo4j = require('neo4j-driver');
+    const driver = neo4j.driver(`bolt://${neo4j_ip_addr}:${neo4j_port}`, neo4j.auth.basic(username, password));
+    const session = driver.session();
+    try { // fetch res from ndeo4j
+        const results = await session.run(query, {});
+        ctx.body = results;
+    } finally {
+        await session.close();
     }
-    ifaces[dev].forEach(function(details) {
-        if (details.family == 'IPv4') {
-            var adr = details.address
-                // console.log(adr)
-            myHost = adr;
-            return adr;
-
-        }
-    });
-}
-getIPAdd();
-
-router.get('/all', (ctx, next) => {
-    // console.log(myHost);
-    var client_list = new Set();
-    var clientHost = ctx.req.connection.remoteAddress.slice(7, );
-    // console.log(ctx.req.connection.remoteAddress.slice(7, ));
-
-    // console.log(client_list);
-    // console.log(ctx.req.headers.origin);
-    // 从主页访问的情况
-    if (ctx.req.headers.origin) {
-        if (client_list.has(clientHost) == false) {
-            client_list.add(clientHost);
-        }
-        query = ctx.query;
-        // console.log(ctx.query);
-    }
-    // 本机访问3000
-    else if (clientHost.includes(myHost) | clientHost == '::1') {
-        console.log(clientHost + '正在访问3000端口并请求');
-        query = ctx.query;
-
-    } else {
-        console.log(clientHost + '正在访问3000端口并请求，已返回空');
-        query = { query: 'MATCH (n:Error) RETURN n' };
-    }
-
-    // console.log(query);
-    return all.allnodesPromise(query.query).then(nodes => {
-        ctx.body = nodes
-    })
-});
-app.use(async(ctx, next) => {
-    try {
-        await next();
-    } catch (e) {
-        console.log(e)
-    }
+    await driver.close();
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen(3000, getIPAdd());
+app.listen(listen_port, neo4j_ip_addr); console.log("listenning: " + 3000);
---
+++ frontend/main.js
@@ -0,0 +1,177 @@
+/**
+* global config 
+*/
+const url = "http://172.29.236.17:3000"
+
+const color_set = ["#dd6b66",
+    "#759aa0",
+    "#e69d87",
+    "#8dc1a9",
+    "#ea7e53",
+    "#eedd78",
+    "#73a373",
+    "#73b9bc",
+    "#7289ab",
+    "#91ca8c",
+    "#f49f42"
+]
+
+const stack = [];
+
+// return a graph
+const graph = (() => {
+
+    // create a G6 graph instance 
+    const g = (() => {
+        /*         G6.registerBehavior('node-activate', {
+    
+                getDefaultCfg() {
+                    return {
+                        multiple: true
+                    };
+                },
+            }); */
+
+        const graph = new G6.Graph({
+            container: 'mountNode',
+            width: window.screen.availWidth,
+            height: document.body.clientHeight,
+
+            modes: {
+                default: ['drag-node', 'node-activate'],
+            },
+
+            layout: {
+                type: 'force',
+                center: [window.screen.availWidth * 0.45, document.body.clientHeight * 0.4],
+                preventOverlap: true,
+                linkDistance: 180,
+            },
+
+            defaultNode: {
+                size: 28,
+                color: '#5B8FF9',
+                style: {
+                    lineWidth: 2,
+                    fill: '',
+                    stroke: '',
+                },
+                label: 'node-label',
+                labelCfg: {
+                    position: 'top',
+                    style: {
+                        fill: '#ddd',
+                    },
+                }
+
+            },
+            defaultEdge: {
+                size: 1,
+                color: '#aaa',
+                label: 'node-label',
+                labelCfg: {
+                    style: {
+                        fill: '#ddd',
+                        stroke: '',
+                    },
+                },
+            }
+        });
+
+        function updateNodePostion(e) {
+            const model = e.item.get('model');
+            model.fx = e.x;
+            model.fy = e.y;
+        }
+
+        graph.on('node:dragstart', (e) => {
+            graph.layout();
+            updateNodePostion(e);
+        });
+        graph.on('node:drag', (e) => {
+            updateNodePostion(e);
+        });
+
+        return graph;
+    })();
+
+    // install alice methods on g
+    g.alice = async (para) => {
+        const q = 'MATCH (n{name:"' + para + '"})-[a]-(x) RETURN n,labels(n),a,type(a),x,labels(x) LIMIT 100';
+        // n returns a object
+        // labels return a array
+        // type returns a string
+
+        // set node style 
+        function setStyle(data) { // data is G6 data = {nodes, edges}
+            const nodes = data.nodes;
+            nodes.forEach((node) => {
+                node.style = {
+                    fill: color_set[node.id % color_set.length], // random color...
+                    stroke: '',
+                };
+                switch (node.type) {
+                    case ('IP' || 'Mail'):
+                        {
+                            node.size = 48;
+                            break;
+                        }
+                    case 'DLL' || 'URL':
+                        {
+                            node.size = 36;
+                            break;
+                        }
+                }
+            });
+        }
+
+        // transform from res to G6 data
+        function transform(res) {
+            nodes = [];
+            edges = [];
+
+            const records = res.data.records;
+
+            const subject = {
+                id: '0',
+                label: records[0]._fields[0].properties.name,
+                type: records[0]._fields[1][0]
+            }
+            nodes.push(subject);
+
+            for (let i = 1; i < records.length; i++) {
+                edges.push({
+                    source: '0',
+                    target: i.toString(),
+                    label: records[i]._fields[3]
+                });
+                nodes.push({
+                    id: i.toString(),
+                    label: records[i]._fields[4].properties.name,
+                    type: records[i]._fields[5][0]
+                })
+            }
+
+            return {
+                'nodes': nodes,
+                'edges': edges,
+            };
+        }
+
+        // render the graph
+        const res = await axios.get(`${url}?query=${q}`);
+        const data = transform(res); // tranform from res to G6 data
+        setStyle(data);
+        graph.data(data);
+        graph.render();
+    }
+
+    // register node:click behavior on g
+    g.on('node:click', (e) => {
+        g.alice(e.item.getModel().label);
+    });
+
+    return g;
+})();
+
+graph.alice(new URLSearchParams(window.location.search).get('name'));
